1eff4b270c31709e620043fff9676c5b32164d37
Joohyun Park <jhp527@nyu.edu>
2020-02-15 23:51:43 -0500

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './docs'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
     },
     plugins: [
